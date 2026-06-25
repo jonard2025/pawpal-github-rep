46d16a29e9199f8cad994756abf2e0997fd89e64
jonard2025 <jonard20javier@gmail.com>
2026-06-25 13:12:12 +0800

    Update pawpals

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/dogs")
+def dogs():
+    return render_template("dogs.html")
+
+@app.route("/cats")
+def cats():
+    return render_template("cats.html")
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(debug=True)		
